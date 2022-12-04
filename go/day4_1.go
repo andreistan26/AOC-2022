4add3b1fd5262cf1451ca897a23f27f85442eb24
andreistan26 <andreistan2003@gmail.com>
2022-12-04 21:24:38 +0200

    Day 4

---
+++ go/day4_1.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+    "fmt"
+    "os"
+    "bufio"
+)
+
+func main(){
+    readFile, _ := os.Open("../res/input_4.txt")
+    fileScanner := bufio.NewScanner(readFile)
+    fileScanner.Split(bufio.ScanLines)
+    sum := 0
+    for fileScanner.Scan(){
+        var g1 , g2[3]int
+        fmt.Sscanf(fileScanner.Text(), "%d-%d,%d-%d", &g1[0], &g1[1], &g2[0], &g2[1])
+        if (g1[0] <= g2[0] && g2[1] <= g1[1]) || (g2[0] <= g1[0] && g1[1] <= g2[1]){
+            sum ++;
+        }
+    }
+    fmt.Println(sum)
+}
